70b5b009cbe2db3a761289a1ac96b0735bc5db94
Thor E Johansson <tjkenmate360@outlook.com>
2022-11-02 15:29:44 -0400

    Removed Yarn, combined cra and express :(

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const port = 3001;
 
 const app = express();
 
+app.use(express.json());
 app.listen(port, () => {
-    console.log(`Sever running on ${port}`);
+  console.log(`Sever running on ${port}`);
 });
