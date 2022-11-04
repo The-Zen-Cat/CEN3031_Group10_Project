c6c94d265cd39f5027442ceb26f8b6e66012d727
kptil <ktiley@ufl.edu>
2022-11-04 19:25:48 -0400

    working navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
+import { Routes, Route } from "react-router-dom";
+import SignUp from './components/signup';
+import LogIn from './components/login';
+import Home from './components/home';
+import About from './components/about';
+
+// it works! start making it look nice. And it needs to default to the home page, may not be.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. but what if i dont
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "About" element = {<About />} />
+        <Route path = "SignUp" element = {<SignUp />} />
+        <Route path = "LogIn" element = {<LogIn />} />
+      </Routes>
+      <div
+        className="head"
+        style={{
+        width: "fit-content",
+        margin: "auto",
+        }}
+      >
+        <h1
+        style={{
+          color: "orange",
+        }}
+        >
+        Picture a Great Website Here!
+        </h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        
+        <div>
+            <h1>Welcome to the Homeless Helper Home!</h1>
+        </div>
+        
+    );
+}
+
+export default Home;
