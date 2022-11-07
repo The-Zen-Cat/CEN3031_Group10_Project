7e63e07a2bc6f004bca6bf0fc04d74058312ed43
Thor E Johansson <tjkenmate360@outlook.com>
2022-11-07 11:15:35 -0500

    Update accountModel.js

--- server/model/accountModel.js
+++ server/model/accountModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Account = mongoose.model(
   'Account',
   new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     types: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Type' }]
   })
