4c212528e728aeb01eac42b20ad8a4b8230f0745
kptil <ktiley@ufl.edu>
2022-11-15 08:31:33 -0500

    edit welcome phrase

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         
         <div>
-            <h1>Welcome to the Homeless Helper Home!</h1>
+            <h1>Welcome to the Homeless Helper!</h1>
             <SearchANDListView />
         </div>
         
