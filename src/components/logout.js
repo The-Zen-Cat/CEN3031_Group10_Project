837d41fde0bbec54e7cba06cedc78b2f04f95d71
kptil <ktiley@ufl.edu>
2022-11-20 22:05:11 -0500

    logout page

---
+++ src/components/logout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LogOut = () => {
+    return (
+        
+        <div>
+            <h1>Output goodbye message, include return to homepage link, change user status?</h1>
+        </div>
+        
+    );
+}
+
+export default LogOut;
